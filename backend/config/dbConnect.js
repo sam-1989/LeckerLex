38de4cfce7b80e852052ed9d78537e2ba047ebbe
Patrik Zlatic <patrik.zlatic@gmail.com>
2024-12-12 13:48:40 +0100

    Add structure tree

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+mongoose.connection.on('error', (error) => {
+    console.log('DB Fehler nach initialer Verbindung:', error);
+});
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            dbName: process.env.DATABASE
+        });
+        console.log('Connected to MongoDB!');
+    } catch (error) {
+        console.error('Connection error:', error);
+    }
+};
+
+export default connectDB;
