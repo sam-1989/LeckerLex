6217b99692ff3eec74726bf72e7e1a9447a89358
Samuel Gitman <sam.gitman@gmail.com>
2025-01-14 09:52:43 +0100

    add aspect-ratio to ingredients gallery

--- frontend/src/components/IngredientsGallery.jsx
+++ frontend/src/components/IngredientsGallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function IngredientsGallery({ herbsAndSpices,
+function IngredientsGallery({ 
+  herbsAndSpices,
   vegetables,
   fruits,
   meat,
@@ -59,32 +60,34 @@ function IngredientsGallery({ herbsAndSpices,
         itemsToDisplay = [];
     }
   return (
-    <div className="grid gap-4 p-4 grid-cols-3 lg:grid-cols-6">
+    <div className="container mx-auto px-4 lg:px-56 pt-2">
+    <div className="grid gap-4 p-8 grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-5">
       {itemsToDisplay.map((item, idx) => {
         const isSelected = selectedIngredients.includes(item.alt);
 
         return (
           <div
             key={idx}
-            className="relative cursor-pointer group overflow-hidden rounded-3xl shadow-inner bg-gray-200 outline-3"
+            className="relative cursor-pointer aspect-w-1 aspect-h-1 group overflow-hidden rounded-3xl shadow-inner bg-gray-200 outline-3"
             onClick={() => handleImageClick(item.alt)}
           >
             <img
               src={item.src}
               alt={item.alt}
-              className="w-full h-full object-cover mx-auto transition-transform duration-300 transform group-hover:scale-105"
+              className="w-full h-full object-contain mx-auto transition-transform duration-300 transform group-hover:scale-105"
             />
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 text-white text-sm sm:text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
               {item.alt}
             </div>
             {isSelected && (
-              <div className="absolute top-2 right-2 text-green-500 text-xl rounded-full p-1 bg-white">
+              <div className="absolute top-0 right-0 text-white text-md rounded-full px-0 bg-white">
                 ✔
               </div>
             )}
           </div>
         );
       })}
+    </div>
     </div>
   );
 }
